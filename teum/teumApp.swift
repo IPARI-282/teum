fce621a55b4cb4d93e4f27716e318debb1e64989
dev-junehee <dev.junehee@gmail.com>
2025-04-06 17:04:28 +0900

    Initial Commit

---
+++ teum/teumApp.swift
@@ -0,0 +1,17 @@
+//
+//  teumApp.swift
+//  teum
+//
+//  Created by junehee on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct teumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
