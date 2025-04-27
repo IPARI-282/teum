dbb5acc40d8c8bcfca349b5c2db2c98f84b29f68
jayden <yoonwoo4429@kakao.com>
2025-04-27 21:43:05 +0900

    [Fix]리베이스 컨플릭 해결

--- teum/ContentView.swift
+++ teum/ContentView.swift
@@ -9,40 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var loginViewModel = LoginViewModel()
-
+    @EnvironmentObject var coordinator: AppCoordinator<Destination>
+    
     var body: some View {
-        if loginViewModel.isLoading {
-            // 로딩 화면 표시
-            ProgressView("로딩 중...")
-                .progressViewStyle(CircularProgressViewStyle())
-        } else if loginViewModel.isAuthenticated {
-            TabView {
-                LookingForTeumView()
-                    .tabItem {
-                        Image(systemName: "1.square.fill")
-                        Text("틈찾기")
-                    }
-                CommunityView()
-                    .tabItem {
-                        Image(systemName: "2.square.fill")
-                        Text("커뮤니티")
-                    }
-                TeumNoteView()
-                    .tabItem {
-                        Image(systemName: "3.square.fill")
-                        Text("틈노트")
-                    }
-                MyPageView()
-                    .tabItem {
-                        Image(systemName: "4.square.fill")
-                        Text("마이페이지")
-                    }
+        NavigationStack(path: $coordinator.paths) {
+            if loginViewModel.isLoading {
+                ProgressView("로딩 중...")
+            } else if loginViewModel.isAuthenticated {
+                TabView {
+                    LookingForTeumView()
+                        .tabItem { Label("틈찾기", systemImage: "1.square.fill") }
+                    CommunityView()
+                        .tabItem { Label("커뮤니티", systemImage: "2.square.fill") }
+                    FireStoreTestView()
+                        .tabItem { Label("틈노트", systemImage: "3.square.fill") }
+                    MyPageView()
+                        .tabItem { Label("마이페이지", systemImage: "4.square.fill") }
+                }
+            } else {
+                LoginView(viewModel: loginViewModel)
             }
-            
-        } else {
-            LoginView(viewModel: loginViewModel)
         }
-        
+        .navigationDestination(for: Destination.self) { destination in
+            switch destination {
+            case .community:
+                CommunityView()
+            case .myPage:
+                MyPageView()
+            case .teumNote:
+                FireStoreTestView()
+            case .lookingForTeum:
+                LookingForTeumView()
+            case .login:
+                LoginView(viewModel: loginViewModel)
+            }
+        }
     }
 }
 
